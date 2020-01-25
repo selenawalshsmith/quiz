a2f0e21c5a613dd6d7b515a090a9e8c9aa8b06d7
Selena <selenawalshsmith@gmail.com>
2020-01-24 16:36:14 -0800

    x

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const startButton = document.getElementById('start-btn')
-startButton.addEventListener('click', startGame);
+startButton.addEventListener('click', startGame)
 
 function startGame(){
   console.log('Started')
