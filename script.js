45d9c041560e0cc26af0a1b8ea11a899313334ae
Selena <selenawalshsmith@gmail.com>
2020-01-29 11:18:03 -0800

    x

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 const startButton = document.getElementById('start-btn')
-//const questionContainerElement = document.getElementbyId('question-container')
+const questionContainerElement = document.getElementbyId('question-container')
+
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame(){
   console.log('Started')
-//  startButton.classList.add('hide')
-  //questionContainerElement.classList.remove('hide')
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
 function setNextQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question) {
+  questionElement.innerText = question.question
 }
 
 function selectAnswer() {
 
 }
+
+const questions = [
+  {
+    question: 'What is 2+2?',
+    answers: [
+      { text: '4', correct: true },
+      { text: '22', correct: false }
+    ]
+  }
+]
