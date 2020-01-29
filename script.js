98638da5d9f0bd345036885a2a5d5ed41a3ad817
Selena <selenawalshsmith@gmail.com>
2020-01-29 12:50:19 -0800

    x

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
 const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
 
-/*
+
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
 let shuffledQuestions, currentQuestionIndex;
-*/
+
 startButton.addEventListener('click', startGame);
 
 function startGame(){
   console.log('Started');
   startButton.classList.add('hide');
-  //shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-  //currentQuestionIndex = 0;
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
   questionContainerElement.classList.remove('hide');
-  //setNextQuestion();
+  setNextQuestion();
 }
 
 function setNextQuestion() {
- //showQuestion(shuffledQuestions[currentQuestionIndex]);
+  resetState();
+ showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
-  //questionElement.innerText = question.question;
+  questionElement.innerText = question.question;
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innrerText = answer.txt;
+    button.classList.add('btn')
+    if (answer.correct){
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener('click', selectAnswer);
+    answerButtonsElement.appendChild(button);
+  })
+}
+
+function resetState(){
+  nextButton.classList.add('hide');
+  while (answerButtonsElement.firstChild){
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
 }
 
 function selectAnswer() {
 
 }
-/*
+
 const questions = [
   {
     question: 'What is 2+2?';
@@ -38,4 +57,3 @@ const questions = [
     ]
   }
 ]
-*/
