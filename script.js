af73b677a57c24acb9d5c56e93ae0e3dcd18bb42
Selena <selenawalshsmith@gmail.com>
2020-01-28 16:20:38 -0800

    x

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementbyId('question-container')
+
 startButton.addEventListener('click', startGame)
 
 function startGame(){
   console.log('Started')
+  startButton.classList.add('hide')
 }
 
 function setNextQuestion() {
