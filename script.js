79f5d6060f74564bbbaa9eee6ca98e8d8cfcc2e0
Selena <selenawalshsmith@gmail.com>
2020-01-28 16:24:35 -0800

    x

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const startButton = document.getElementById('start-btn')
-const questionContainerElement = document.getElementbyId('question-container')
+const startButton = document.getElementByID('start-btn')
+//const questionContainerElement = document.getElementbyId('question-container')
 
 startButton.addEventListener('click', startGame)
 
 function startGame(){
   console.log('Started')
-  startButton.classList.add('hide')
+//  startButton.classList.add('hide')
+  //questionContainerElement.classList.remove('hide')
 }
 
 function setNextQuestion() {
