47788ff4b6e0d2d50725aa42ee5b2900efe07fe1
Selena <selenawalshsmith@gmail.com>
2020-01-29 12:59:19 -0800

    x

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function selectAnswer() {
 
 const questions = [
   {
-    question: 'What is 2+2?';
+    question: 'What is 2+2?'
     answers: [
       { text: '4', correct: true },
       { text: '22', correct: false }
